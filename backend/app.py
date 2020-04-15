a346135b30bf67bff4e50fddf96081a8f327faeb
thaison1496 <thaison1496@gmail.com>
2020-04-15 17:50:31 +0700

    add sql alchemy

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,13 @@
+import os
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
 
 @app.route('/api/hello', methods=['GET'])
 def index():
